2c9209084f3acafd15048ed2a8e7a9993a3ab216
Gustavo Maldonado <gamcba7982@gmail.com>
2023-01-17 16:44:52 -0300

    setup maps marker

--- app/src/main/java/com/mymaps/MainActivity.kt
+++ app/src/main/java/com/mymaps/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.mymaps
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createFragment()
+    }
+
+    private fun createFragment() {
+        val mapFragmnet: SupportMapFragment =
+            supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragmnet.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker()
+    }
+
+    private fun createMarker() {
+        val coordinates = LatLng(-31.416838, -64.183583)
+        val marker: MarkerOptions = MarkerOptions().position(coordinates).title("Mi plaza favorita")
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates, 16f),
+            4000,
+            null
+        )
     }
 }
